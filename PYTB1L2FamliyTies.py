477dc2edcf9211b0a44c09c8fd6e15de9cdcc583
shihua <liushihua2004@gmail.com>
2020-10-23 11:51:02 +0200

    fouten verbeterd

--- PYTB1L2FamliyTies.py
+++ PYTB1L2FamliyTies.py
@@ -1,6 +1,6 @@
 naam = 'shihua'
 leeftijd = 16
-afstand = '41,44'
+afstand = 41.44
 woonplaats = 'hoorn'
 
 print("mijn naam is " + naam + " .\nIk ben " + str(leeftijd) + " en ik woon in " + woonplaats + ".\nIk woon " + str(afstand) + " km van het MediaCollege.")
--- pytb1l6 distribution.py
+++ pytb1l6 distribution.py
@@ -8,7 +8,27 @@ def opnieuw():
 def leeg():
     print("fabriek[]\ndistributie[]\nwinkel[]")
 
-def fabriek():
+def doe_winkel(fabriek,distributie,winkel):
+    winkel.append("motor")
+    os.system("cls")
+    print("fabriek" + str(fabriek) + "\ndistributie" + str(distributie) + "\nwinkel" + str(winkel))
+    time.sleep(1)
+    winkel.remove("motor")
+    os.system("cls")
+    print("fabriek" + str(fabriek) + "\ndistributie" + str(distributie) + "\nwinkel" + str(winkel))
+
+
+def doe_distributie(fabriek,distributie,winkel):
+    distributie.append("motor")
+    os.system("cls")
+    print("fabriek" + str(fabriek) + "\ndistributie" + str(distributie) + "\nwinkel" + str(winkel))
+    time.sleep(1)
+    distributie.remove("motor")
+    os.system("cls")
+    print("fabriek" + str(fabriek) + "\ndistributie" + str(distributie) + "\nwinkel" + str(winkel))
+    doe_winkel(fabriek,distributie,winkel)
+
+def doe_fabriek():
     fabriek = []
     distributie = []
     winkel = []
@@ -19,35 +39,10 @@ def fabriek():
     fabriek.remove("motor")
     os.system("cls")
     print("fabriek" + str(fabriek) + "\ndistributie" + str(distributie) + "\nwinkel" + str(winkel))
-
-def distributie():
-    fabriek = []
-    distributie = []
-    winkel = []
-    distributie.append("motor")
-    os.system("cls")
-    print("fabriek" + str(fabriek) + "\ndistributie" + str(distributie) + "\nwinkel" + str(winkel))
-    time.sleep(1)
-    distributie.remove("motor")
-    os.system("cls")
-    print("fabriek" + str(fabriek) + "\ndistributie" + str(distributie) + "\nwinkel" + str(winkel))
-
-def winkel():
-    fabriek = []
-    distributie = []
-    winkel = []
-    winkel.append("motor")
-    os.system("cls")
-    print("fabriek" + str(fabriek) + "\ndistributie" + str(distributie) + "\nwinkel" + str(winkel))
-    time.sleep(1)
-    winkel.remove("motor")
-    os.system("cls")
-    print("fabriek" + str(fabriek) + "\ndistributie" + str(distributie) + "\nwinkel" + str(winkel))
+    doe_distributie(fabriek,distributie,winkel)
 
 def alles():
-    fabriek()
-    distributie()
-    winkel()
+    doe_fabriek()
     os.system("cls")
     print("fabriek[]\ndistributie[]\nwinkel[]\nwilt u opnieuw beginnen?   Y/N")
 
